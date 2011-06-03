e0dcc1ddfb6887dd47fb8f5cb300db733478ba79
Mat Schaffer <mat@schaffer.me>
2011-06-02 20:59:04 -0400

    Switch to vundle
    See https://github.com/gmarik/vundle for info.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,31 +5,31 @@ set -e
 test -f ~/.vimrc && mv ~/.vimrc ~/.vimrc.bak
 
 cat <<-VIM > ~/.vimrc
-call pathogen#helptags()
-call pathogen#runtime_append_all_bundles()
+set nocompatible
+filetype off
+
+set rtp+=~/.vim/bundle/vundle/
+call vundle#rc()
+
+Bundle 'gmarik/vundle'
+Bundle 'matschaffer/vim-matschaffer'
+Bundle 'tpope/vim-fugitive'
+Bundle 'tpope/vim-rails'
+Bundle 'tpope/vim-cucumber'
+Bundle 'tpope/vim-haml'
+Bundle 'bbommarito/vim-slim'
+Bundle 'mileszs/ack.vim'
+Bundle 'vim-scripts/pbcopy.vim'
+Bundle 'kchmck/vim-coffee-script'
+Bundle 'altercation/vim-colors-solarized'
+
+Bundle 'JavaScript Indent'
+
+filetype plugin indent on
 VIM
 
-mkdir -p ~/.vim/{autoload,bundle,backup}
+mkdir -p ~/.vim/{bundle,backup}
 
-curl -s https://github.com/tpope/vim-pathogen/raw/master/autoload/pathogen.vim > ~/.vim/autoload/pathogen.vim
-
-git_bundle() {
-  local url="$1"
-  local base="$HOME/.vim/bundle"
-  local name=`echo "$url" | awk -F/ '{print $NF}' | sed 's/.git$//'`
-  local directory="$base/$name"
-  if [[ -d "$directory" ]]; then
-    echo "Updating $url..."
-    cd "$directory"
-    git pull
-    cd - > /dev/null
-  else
-    echo "Retrieving $url..."
-    git clone "$url" "$directory"
-  fi
-}
-
-git_bundle https://github.com/tpope/vim-rails.git
-git_bundle https://github.com/matschaffer/vim-matschaffer.git
-git_bundle https://github.com/tpope/vim-fugitive.git
-git_bundle https://github.com/mileszs/ack.vim.git
+if ! [[ -d ~/.vim/bundle/vundle ]]; then
+  git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+fi
