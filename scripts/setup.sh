5b301ee880982c09241011a0bc189edfa8a96142
Mat Schaffer <mat@schaffer.me>
2012-01-10 23:32:00 -0500

    Update bundle list

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -13,19 +13,21 @@ set rtp+=~/.vim/bundle/vundle/
 call vundle#rc()
 
 Bundle 'gmarik/vundle'
-Bundle 'matschaffer/vim-matschaffer'
-Bundle 'tpope/vim-fugitive'
-Bundle 'tpope/vim-rails'
-Bundle 'tpope/vim-cucumber'
-Bundle 'tpope/vim-haml'
-Bundle 'bbommarito/vim-slim'
-Bundle 'mileszs/ack.vim'
 Bundle 'vim-scripts/pbcopy.vim'
 Bundle 'kchmck/vim-coffee-script'
 Bundle 'altercation/vim-colors-solarized'
 Bundle 'plasticboy/vim-markdown'
+Bundle 'wincent/Command-T'
+Bundle 'vim-ruby/vim-ruby'
+Bundle 'tpope/vim-fugitive'
 
 Bundle 'Simple-Javascript-Indenter'
+Bundle 'closetag.vim'
+
+Bundle 'VimClojure'
+Bundle 'jpalardy/vim-slime'
+
+Bundle 'matschaffer/vim-matschaffer'
 
 filetype plugin indent on
 VIM
