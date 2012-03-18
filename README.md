05496fca09a4f8bb827c97fe0da42df21a480fdd
Mat Schaffer <mat@schaffer.me>
2012-03-18 00:45:16 -0400

    Support prefix and auto BundleInstall

--- README.md
+++ README.md
@@ -4,10 +4,6 @@
 
 NOTE: This will move your existing .vimrc to .vimrc.bak and create a new .vimrc
 
+ALSO NOTE: This assumes your default Vim is available from the `vi` command. If this is not the case, run `:BundelInstall` inside vim after installing.
+
     bash < <(curl -Ls https://github.com/matschaffer/vim-matschaffer/raw/master/scripts/setup.sh)
-
-Once in vim run
-
-    :BundleInstall
-
-Then restart vim and you'll be good to go.
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,14 +4,22 @@ set -e
 
 mkdir -p ~/.vim/{bundle,backup}
 
+if [[ -n "$RW" ]]; then
+  PREFIX="git@github.com:"
+else
+  PREFIX="https://github.com/"
+fi
+
 if ! [[ -d ~/.vim/bundle/vundle ]]; then
   git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 fi
 
 if ! [[ -d ~/.vim/bundle/vim-matschaffer ]]; then
-  git clone http://github.com/matschaffer/vim-matschaffer.git ~/.vim/bundle/vim-matschaffer
+  git clone ${PREFIX}matschaffer/vim-matschaffer.git ~/.vim/bundle/vim-matschaffer
 fi
 
 test -f ~/.vimrc && mv ~/.vimrc ~/.vimrc.bak
 
 ln -s ~/.vim/bundle/vim-matschaffer/vimrc ~/.vimrc
+
+vi +BundleInstall +qall
